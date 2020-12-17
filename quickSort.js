0eda433c364ad4c31d8269ec370e97666703d6ec
sgbliznyuk <sgbliznyuk@gmail.com>
2020-12-17 19:31:49 +0300

    added quick sort with random index

--- quickSort.js
+++ quickSort.js
@@ -13,3 +13,24 @@ function quickSort(array) {
 }
 
 console.log(quickSort([1,3,4,5,7,3,44,5,6,23,45,89,1,0]))
+
+//quick sort with random index
+
+/*
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    } else {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        const pivot = arr[randomIndex];
+        const less = arr.slice(0).filter((el, k) => el < pivot && k !== randomIndex);
+        const greater = arr.slice(0).filter((el, k) => el >= pivot && k !== randomIndex);
+
+        return [...quickSort(less), pivot, ...quickSort(greater)];
+    }
+}
+
+console.log(quickSort([2,3,4,435,345,22,5,6,7,4,53,4,4,32,234,234,2,565,645,64]))
+
+*/
