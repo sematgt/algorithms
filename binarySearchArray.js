253a3f638b5cb16a9278a72828ada521ab0220e8
sgbliznyuk <sgbliznyuk@gmail.com>
2020-12-17 19:13:51 +0300

    error fix

--- binarySearchArray.js
+++ binarySearchArray.js
@@ -1,25 +1,26 @@
 // O(log(n)) time complexity
 // O(n) space complexity
 
-function binarySearch(array, item) {
-    let first = 0;
-    let middle;
-    let last = array.length - 1;
-    while (first < last) {
-        middle = Math.floor((last - first) / 2) + first;
-        if (item === array[middle]) {
-            return middle;
-        } else if (item < array[middle]) {
-            last = middle;
+function binarySearch(arr, n) {
+    let low = 0;
+    let high = arr.length - 1;
+    
+    while (low <= high) {
+        let mid = Math.floor((high - low)/2) + low;
+
+        if (n === arr[mid]) return mid;
+
+        if (n > arr[mid]) {
+            low = mid + 1;
         } else {
-            first = middle;
+            high = mid - 1;
         }
-        console.log(first, middle, last)    
     }
 
-    return first;
+    return -1;
 }
 
-console.log(binarySearch([1,2,4,6,8,9,11,13,15,16], 11) === 6)
-console.log(binarySearch([1,2,4,6,8,9,11,13,15,16], 15) === 8)
-console.log(binarySearch([1,2,4,6,8,9,11,13,15,16], 2) === 1)
+console.log(binarySearch([1,2,4,5,6,8,15], 5),
+binarySearch([1,2,4,5,6,8,15], 1),
+binarySearch([1,2,4,5,6,8,15], 15),
+binarySearch([1,2,4,5,6,8,15], 5222))
